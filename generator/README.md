710e04ebfb2e1c4f5383350ba48bb86721df9c10
madhvesh-77 <madhveshs@sahaj.ai>
2026-08-06 20:19:04 +0530

    Add reusable PostgreSQL connection module

---
+++ generator/README.md
@@ -0,0 +1,14 @@
+# Generator Module
+
+This module simulates business events for the ShopSmart application.
+
+Current Features
+
+- PostgreSQL Connection
+
+Upcoming
+
+- Customer Generator
+- Product Generator
+- Order Generator
+- Black Friday Simulation
---
+++ generator/db.py
@@ -0,0 +1,43 @@
+from dotenv import load_dotenv
+import os
+import psycopg
+
+load_dotenv()
+
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT")
+DB_NAME = os.getenv("DB_NAME")
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+
+
+def get_connection():
+    return psycopg.connect(
+        host=DB_HOST,
+        port=DB_PORT,
+        dbname=DB_NAME,
+        user=DB_USER,
+        password=DB_PASSWORD,
+    )
+
+
+def test_connection():
+
+    connection = get_connection()
+
+    cursor = connection.cursor()
+
+    cursor.execute("SELECT version();")
+
+    version = cursor.fetchone()
+
+    print(version[0])
+
+    cursor.close()
+
+    connection.close()
+
+
+if __name__ == "__main__":
+
+    test_connection()
